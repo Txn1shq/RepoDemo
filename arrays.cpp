bf898c9495da0ceddbcfa76d316a50de9df07293
Txn1shq <sharmatanishq891@gmail.com>
2024-07-21 21:46:31 +0530

    new learnings

---
+++ arrays.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+/*
+int main() {
+    int arr[5];
+    cin>>arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4];
+    
+    arr[3] += 10;
+    cout<<arr[3]<<'\n';
+
+    return 0;
+}
+*/
+
+// 2d arrays
+ int main() {
+    int arr[3][5];
+
+    arr[1][3] = 78;
+    cout<<arr[1][3]<<endl<<arr[0][4]<<endl;
+
+
+    return 0;
+ }
---
+++ string.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    string s = "tanishq";
+    cout<<s[0]<<endl<<s[1]<<endl;
+    int length = s.size();
+    cout<<s[length - 1]<<endl;
+
+//string stores characters within itself
+//each data below is a char hence used single inverted commas
+
+    s[length -1] = 'k';
+    cout<<s[length -1]<<endl;
+
+
+    return 0;
+}
