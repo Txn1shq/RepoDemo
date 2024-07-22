0bf7a049b63ceeaa3d44eed481f536fc64ded591
Txn1shq <sharmatanishq891@gmail.com>
2024-07-23 00:00:02 +0530

    new learnings

--- functions.cpp
+++ functions.cpp
@@ -7,13 +7,19 @@ parameterised
 non parameterised 
 */
 void printName(string name) {
-    cout<<"hey " <<name;
+    cout<<"hey " <<name<<endl;
 }
 
 int main() {
     string name;
+    cout<<"entr first name: ";
     cin >> name;
     printName(name);
+
+    string name2;
+    cout<<"enter second name: ";
+    cin >>name2;
+    printName(name2);
 
     return 0;
 }
